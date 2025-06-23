2f26a4377ae32ebb2af27f3184a25678a7fd03b8
Shivam24S <shivamshrivastav8055@gmail.com>
2025-06-23 18:21:30 +0530

    find total program

---
+++ 1.code execution.js
@@ -0,0 +1,12 @@
+var n = 2;
+
+function square(n) {
+  const result = n * n;
+  return result;
+}
+
+var square2 = square(n);
+
+var square4 = square(4);
+
+console.log(square2, square4);
---
+++ 2.findSum.js
@@ -0,0 +1,12 @@
+const num = [1, 2, 3, 4, 5, 8, 9, 10];
+
+const findSum = (num) => {
+  let sum = 0;
+
+  for (let i = 0; i < num.length; i++) {
+    sum = num[i] + sum;
+  }
+  console.log(sum);
+};
+
+findSum(num);
