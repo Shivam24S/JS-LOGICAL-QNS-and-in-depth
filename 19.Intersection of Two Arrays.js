0357125758cc63f2399996ec12312e3877965493
Shivam24S <shivamshrivastav8055@gmail.com>
2025-06-29 12:52:06 +0530

    intersection of two array.js

---
+++ 19.Intersection of Two Arrays.js
@@ -0,0 +1,42 @@
+const num1 = [1, 2, 3, 4, 5, 6];
+
+const num2 = [2, 7, 8, 3, 4, 9];
+
+// using array Method
+
+const interSection = (num1, num2) => {
+  const uniqueVal = new Set(num2);
+
+  const matched = num1.filter((value) => uniqueVal.has(value));
+
+  console.log("matched val", matched);
+};
+
+interSection(num1, num2);
+
+// using loop
+const interSectionVal = (num1, num2) => {
+  let matchedNum = [];
+
+  for (let i = 0; i < num1.length; i++) {
+    for (let j = 0; j < num2.length; j++) {
+      if (num1[i] === num2[j]) {
+        matchedNum.push(num1[i]);
+        break;
+      }
+    }
+  }
+  console.log("matched Num", matchedNum);
+};
+
+interSectionVal(num1, num2);
+
+// using another method
+
+const interSectionNum = (num1, num2) => {
+  const matchedVal = num1.filter((val) => num2.includes(val));
+
+  console.log("matchedVal", matchedVal);
+};
+
+interSectionNum(num1, num2);
