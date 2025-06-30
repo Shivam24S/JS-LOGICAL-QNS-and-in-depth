eb2f34cfb3dd578a2a3d278f61b1fb1f20affb87
Shivam24S <shivamshrivastav8055@gmail.com>
2025-06-30 11:44:04 +0530

    checking even or odd

---
+++ 21.check even or odd.js
@@ -0,0 +1,11 @@
+const num = 24;
+
+const checkNum = (num) => {
+  if (num % 2 === 0) {
+    console.log(num, "is even number");
+  } else {
+    console.log(num, "is odd number");
+  }
+};
+
+checkNum(num);
